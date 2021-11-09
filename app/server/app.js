b1e1f66aa0e84856dad04ebbc2ee1514feabdb05
Aleksey Zikrackiy <a.zikrackiy@gmail.com>
2021-11-09 11:22:54 +0800

    WIP

--- app/server/app.js
+++ app/server/app.js
@@ -86,6 +86,7 @@ app.get('/ssh/reauth', (req, res) => {
 
 // eslint-disable-next-line complexity
 app.get('/ssh/host/:host?', (req, res) => {
+  console.log(111111111, req.params.host);
   res.sendFile(path.join(path.join(publicPath, 'client.htm')));
   // capture, assign, and validate variables
   req.session.ssh = {
